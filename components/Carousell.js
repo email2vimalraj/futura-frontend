9ac1148a379a992cc80f406d36a6674479983738
Vimalraj Selvam <email2vimalraj@gmail.com>
2020-11-30 15:04:39 +0800

    Fixed responsiveness for the home page

--- components/Carousell.js
+++ components/Carousell.js
@@ -77,17 +77,12 @@ const ImageSlider = () => {
 
 const Carousell = () => {
   return (
-    <section className="home-section relative h-screen">
+    <section className="home-section relative h-screen hidden md:flex">
       <div className="logo absolute w-96 h-16 left-1/2 top-1/2 -ml-48 -mt-8 z-50">
         <Logo />
       </div>
 
       <div className="bg-slider absolute w-full h-full t-0 l-0 z-0">
-        {/* <div
-          style={{ backgroundImage: "url(/home/2x.jpg)" }}
-          className="absolute bg-cover w-full h-full l-0 t-0 bg-center bg-no-repeat"
-        /> */}
-
         <ImageSlider />
       </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -10,68 +10,125 @@ import styles from "./index.module.css";
 const ContentSection = () => {
   const { ref, inView } = useInView();
 
-  console.log(inView);
-
   return (
-    <section className="relative z-10 clearfix inline-block mb-10">
-      <div className="content-section pt-28 pb-36 pl-56 my-0 mx-auto relative w-screen">
-        <div ref={ref} className="weare block float-left clear-right w-2/6">
-          <a
-            href="/"
-            className="relative w-auto h-96 block overflow-hidden p-5"
+    <section className="flex flex-col md:flex-row md:container md:mx-auto space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-20 mb-20 px-4 justify-center">
+      <div
+        ref={ref}
+        className="weare block float-left clear-right w-full md:w-2/6"
+      >
+        <div className="relative w-auto h-96 block overflow-hidden p-5">
+          <div
+            className={`line-1 w-0 top-0 left-0 h-0.5 bg-black absolute ${
+              inView ? styles.inviewHorizontal1 : ""
+            }`}
           >
-            <div
-              className={`line-1 w-0 top-0 left-0 h-0.5 bg-black absolute ${
-                inView ? styles.inviewHorizontal1 : ""
-              }`}
-            >
-              <i className="left-0 top-0 block absolute h-full" />
-            </div>
-            <div
-              className={`line-2 h-0 top-0 right-0 w-0.5 bg-black absolute ${
-                inView ? styles.inviewVertical2 : ""
-              }`}
-            >
-              <i className="left-0 top-0 block absolute w-full" />
-            </div>
-            <div
-              className={`line-3 w-0 right-0 bottom-0 h-0.5 bg-black absolute ${
-                inView ? styles.inviewHorizontal3 : ""
-              }`}
-            >
-              <i className="right-0 top-0 block absolute h-full" />
-            </div>
-            <div
-              className={`line-4 h-0 left-0 bottom-0 w-0.5 bg-black absolute ${
-                inView ? styles.inviewVertical4 : ""
-              }`}
-            >
-              <i className="bottom-0 left-0 block absolute w-full" />
-            </div>
-            <div
-              style={{ backgroundImage: `url(/home/homeAbt.jpg)` }}
-              className="w-full h-full bg-cover bg-no-repeat"
-            />
-          </a>
-        </div>
-
-        <div className="wearetextbx float-left w-6/12 pl-5 pr-5 text-xl text-gray-600 leading-9 text-justify font-futuraBookRegular">
-          <p>
-            With over a decade of rich experience in architectural lighting
-            FUTURA is an established entity in the lighting industry. Our quest
-            is to enhance the inspiration of architects, interior and lighting
-            designers with the best in its class lighting products.
-          </p>
-          <br />
-          <p>
-            We have invested in the areas of product development to enhance
-            product design and technical excellence to bring out a vast range to
-            suit every need. Our in house R&D, Test and Measurement facilities
-            gives added confidence to our customers.
-          </p>
+            <i className="left-0 top-0 block absolute h-full" />
+          </div>
+          <div
+            className={`line-2 h-0 top-0 right-0 w-0.5 bg-black absolute ${
+              inView ? styles.inviewVertical2 : ""
+            }`}
+          >
+            <i className="left-0 top-0 block absolute w-full" />
+          </div>
+          <div
+            className={`line-3 w-0 right-0 bottom-0 h-0.5 bg-black absolute ${
+              inView ? styles.inviewHorizontal3 : ""
+            }`}
+          >
+            <i className="right-0 top-0 block absolute h-full" />
+          </div>
+          <div
+            className={`line-4 h-0 left-0 bottom-0 w-0.5 bg-black absolute ${
+              inView ? styles.inviewVertical4 : ""
+            }`}
+          >
+            <i className="bottom-0 left-0 block absolute w-full" />
+          </div>
+          <div
+            style={{ backgroundImage: `url(/home/homeAbt.jpg)` }}
+            className="w-full h-full bg-cover bg-no-repeat"
+          />
         </div>
       </div>
+
+      <div className="border border-solid border-gray-400 p-4 md:hidden">
+        <img src="/FuturaLogoDark.svg" alt="Futura" />
+      </div>
+
+      <div className="wearetextbx float-left w-full md:w-6/12 pl-5 pr-5 text-xl text-gray-600 leading-9 text-justify font-futuraBookRegular">
+        <p>
+          With over a decade of rich experience in architectural lighting FUTURA
+          is an established entity in the lighting industry. Our quest is to
+          enhance the inspiration of architects, interior and lighting designers
+          with the best in its class lighting products.
+        </p>
+        <br />
+        <p>
+          We have invested in the areas of product development to enhance
+          product design and technical excellence to bring out a vast range to
+          suit every need. Our in house R&D, Test and Measurement facilities
+          gives added confidence to our customers.
+        </p>
+      </div>
     </section>
+
+    // <section className="relative z-10 clearfix inline-block mb-10">
+    //   <div className="content-section pt-28 pb-36 pl-56 my-0 mx-auto relative w-screen">
+    //     <div ref={ref} className="weare block float-left clear-right w-2/6">
+    //       <div className="relative w-auto h-96 block overflow-hidden p-5">
+    //         <div
+    //           className={`line-1 w-0 top-0 left-0 h-0.5 bg-black absolute ${
+    //             inView ? styles.inviewHorizontal1 : ""
+    //           }`}
+    //         >
+    //           <i className="left-0 top-0 block absolute h-full" />
+    //         </div>
+    //         <div
+    //           className={`line-2 h-0 top-0 right-0 w-0.5 bg-black absolute ${
+    //             inView ? styles.inviewVertical2 : ""
+    //           }`}
+    //         >
+    //           <i className="left-0 top-0 block absolute w-full" />
+    //         </div>
+    //         <div
+    //           className={`line-3 w-0 right-0 bottom-0 h-0.5 bg-black absolute ${
+    //             inView ? styles.inviewHorizontal3 : ""
+    //           }`}
+    //         >
+    //           <i className="right-0 top-0 block absolute h-full" />
+    //         </div>
+    //         <div
+    //           className={`line-4 h-0 left-0 bottom-0 w-0.5 bg-black absolute ${
+    //             inView ? styles.inviewVertical4 : ""
+    //           }`}
+    //         >
+    //           <i className="bottom-0 left-0 block absolute w-full" />
+    //         </div>
+    //         <div
+    //           style={{ backgroundImage: `url(/home/homeAbt.jpg)` }}
+    //           className="w-full h-full bg-cover bg-no-repeat"
+    //         />
+    //       </div>
+    //     </div>
+
+    //     <div className="wearetextbx float-left w-6/12 pl-5 pr-5 text-xl text-gray-600 leading-9 text-justify font-futuraBookRegular">
+    //       <p>
+    //         With over a decade of rich experience in architectural lighting
+    //         FUTURA is an established entity in the lighting industry. Our quest
+    //         is to enhance the inspiration of architects, interior and lighting
+    //         designers with the best in its class lighting products.
+    //       </p>
+    //       <br />
+    //       <p>
+    //         We have invested in the areas of product development to enhance
+    //         product design and technical excellence to bring out a vast range to
+    //         suit every need. Our in house R&D, Test and Measurement facilities
+    //         gives added confidence to our customers.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </section>
   );
 };
 
@@ -80,7 +137,7 @@ export default function Home() {
     <Layout>
       <Preloader />
 
-      <div className="social absolute top-10 right-10 whitespace-nowrap flex space-x-3 text-white text-md z-50 clearfix">
+      <div className="social absolute top-10 right-10 whitespace-nowrap space-x-3 text-white text-md z-50 clearfix hidden md:flex">
         <a href="/" title="Facebook" target="_blank">
           <FontAwesomeIcon icon={["fab", "facebook-square"]} />
         </a>
